2f7852b48677a75470f3b8e100f24c65d1e10237
Mark Smith <markjgsmith@gmail.com>
2015-06-25 12:14:16 +0700

    Added error handlers

--- app.js
+++ app.js
@@ -13,6 +13,36 @@ app.set('views', path.join( __dirname, 'views'));
 app.set('view engine', 'ejs');
 app.engine( 'ejs', ejs_locals );
 
+// Setup error handlers
+app.routes_builder.on('setup-complete', function(app) {
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
+
+  // development error handler - will print stacktrace
+  if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('error', {
+        message: err.message,
+        error: err
+      });
+    });
+  }
+
+  // production error handler - no stacktraces leaked to user
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: {}
+    });
+  });
+});
+
 var server = app.listen(app.get('port'), function() {
   console.log('Starting routes-builder-demo-project - Listening on port ' + server.address().port);
 });
